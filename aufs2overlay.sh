acf5478a6b9a0479bfa08e1661ce925d76f24d16
Robert Günzler <r@gnzler.io>
2021-05-19 15:52:45 +0200

    Fix for newer supervisors
    The supervisor changed to read device type and architecture from
    `/mnt/boot/device-type.json` which needs to be copied into place.
    
    This merges the aufs2overlay script into the new entrypoint script as
    well.
    
    Signed-off-by: Robert Günzler <robertg@balena.io>

--- aufs2overlay.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sed -i "s/-s aufs/-s overlay2/g" /lib/systemd/system/balena.service; \
-if [ -f /etc/systemd/system/balena.service.d/balena.conf ]; then \
-    sed -i "s/-s aufs/-s overlay2/g" /etc/systemd/system/balena.service.d/balena.conf; \
-else \
-    echo "No drop-in found. Most probably you don't run a development image."; \
-fi
---
+++ entry.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# switch storage driver to overlay2
+units="/lib/systemd/system/balena.service"
+if [ -f /etc/systemd/system/balena.service.d/balena.conf ]; then
+	units="${units} /etc/systemd/system/balena.service.d/balena.conf"
+fi
+for unit in $units; do
+	sed -i "s/-s aufs/-s overlay2/g" "${unit}"
+done
+
+# copy device-type.json into the place the supervisor looks for it
+cp /resin-boot/device-type.json /mnt/boot/
+
+exec /sbin/init
